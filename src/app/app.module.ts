9d74f15f016e9230fddac1a9977edb0f495d139c
jonathanavalencia <jonathan.valencia@ceiba.com.co>
2018-07-18 11:39:15 -0500

    Design UI. Add components: header, parking, register-vehicle. Create forms and tables to load information.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegisterVehicleComponent } from './components/register-vehicle/register-vehicle.component';
+import { ParkingComponent } from './components/parking/parking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RegisterVehicleComponent,
+    ParkingComponent,
   ],
   imports: [
     BrowserModule
